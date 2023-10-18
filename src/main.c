7d86318f72203004b9055a7fb0eff3e128a067f6
Alexandre Ouellet Fournier <aouellet@c2r4p7.42quebec.com>
2023-10-18 19:02:32 -0400

    envp copy

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmehour <kmehour@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aouellet <aouellet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 10:50:43 by kmehour           #+#    #+#             */
-/*   Updated: 2023/10/18 18:26:31 by kmehour          ###   ########.fr       */
+/*   Updated: 2023/10/18 18:38:40 by aouellet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,34 @@
 
 void test_parsing(char *line);
 
+char **copy_envp(char *const envp[])
+{
+	char **envp_cp;
+	int ptr_i;
+
+	ptr_i = 0;
+	while(envp[ptr_i])
+		ptr_i++;
+		
+	envp_cp = (char**)malloc(sizeof(char*)*ptr_i+1);
+	if(!envp_cp)
+		return NULL;
+		
+	ptr_i = 0;
+	while(envp[ptr_i])
+	{
+		envp_cp[ptr_i] = ft_strdup(envp[ptr_i]);
+		//free if error		
+		ptr_i++;	
+	}
+	envp_cp[ptr_i] = NULL;
+	return envp_cp;
+}
+
 int	main(int argc, char **argv, char *const envp[])
 {
 	char	*line;
+	char	**envp_cp;
 	(void)	argc;
 	(void)	argv;
 	t_exec	*command;
@@ -25,6 +50,9 @@ int	main(int argc, char **argv, char *const envp[])
 	ft_set_signal_actions(SIG_MAIN);
 	while (1)
 	{
+		//	copy envp
+		envp_cp = copy_envp(envp);
+
 		//	Readline
 		line = readline("minishell > ");
 
@@ -39,10 +67,10 @@ int	main(int argc, char **argv, char *const envp[])
 		{
 			add_history(line);
 			//	Parse Input
-			command = ft_parse_input(line, envp);
+			command = ft_parse_input(line, envp_cp);
 			
 			//	Execute Command(s)
-			ft_execute(command, envp);
+			ft_execute(command, envp_cp);
 		}
 		free(line);
 	}
